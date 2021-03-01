ad041e62bdad31a3a628ad53cc80c1d41ef5570c
Ash Sharma <ashrut05@protonmail.com>
2021-03-01 08:05:41 -0500

    Database Connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/business"
+    //"URI": "mongodb://localhost/business"
+    "URI":"mongodb+srv://ash:lK5JCz1r7dbKy78a@mongodbserver.tiafv.mongodb.net/business?retryWrites=true&w=majority"
 }
